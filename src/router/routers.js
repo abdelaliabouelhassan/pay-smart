7c95ceae1b29b73a27601a4fd3a7fe2e8871835e
abdelaliabouelhassan <abdelaliabouelhassan@gmail.com>
2022-10-13 14:54:15 +0100

    coming soon

--- src/router/routers.js
+++ src/router/routers.js
@@ -2,11 +2,17 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Exchange from '../views/Exchange.vue'
 import Custody from '../views/Custody.vue'
+import Atm from '../views/Atm.vue'
+import About from '../views/About.vue'
+import BuyCrypto from '../views/BuyCrypto.vue'
 const routes = [
     { path: '/', component: Home },
     { path: '/home', component: Home,name:'home' },
     { path: '/exchange', component: Exchange,name:'Exchange' },
     { path: '/custody', component: Custody,name:'Custody' },
+    { path: '/atm', component: Atm,name:'Atm' },
+    { path: '/about', component: About,name:'About' },
+    { path: '/buy-crypto', component: BuyCrypto,name:'BuyCrypto' },
 ]
 
 const router = createRouter({
---
+++ src/views/About.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="w-full overflow-hidden  "  >
+        <div class=" w-full ">
+            <img class=" w-full h-full bg-cover" :src="comingsoon" alt=""> 
+        </div>
+         <div class=" absolute top-1/4 left-1/2 right-1/2">
+        <div class=" flex flex-col items-center space-y-8">
+            <span class=" text-[#ECEDEF] text-[32px] font-Inter font-medium whitespace-nowrap">Soon</span>
+            <h1 class=" text-white text-[90px] font-Inter font-semibold whitespace-nowrap">Coming soon</h1>
+            <span class=" text-[#C3C8CD] font-medium font-Inter text-xl whitespace-nowrap">This page will be available soon.</span>
+        </div>
+   </div>
+    </div>
+</template>
+<script>
+import comingsoon from '../assets/comingsoon.png'
+export default {
+    data() {
+        return {
+            comingsoon
+        }
+    },
+    mounted(){
+       if(!document.body.classList.contains('dark')){
+            document.body.classList.add('dark')
+       }
+    },
+    //when route leave remove dark class
+    beforeRouteLeave(to, from, next) {
+        document.body.classList.remove('dark')
+        next()
+    }
+   
+}
+</script>
