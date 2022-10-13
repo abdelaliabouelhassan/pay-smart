97c4d13e8dc572d6aeec32f4ece2c298f04ee6a9
abdelaliabouelhassan <abdelaliabouelhassan@gmail.com>
2022-10-13 15:36:46 +0100

    dark mode icons

--- src/components/partial/header.vue
+++ src/components/partial/header.vue
@@ -198,45 +198,42 @@
             </a>
           </div>
         </div>
-         <label
-          for="checked-toggle"
-          class="inline-flex relative items-center cursor-pointer"
-        >
-          <input
-            type="checkbox"
-            value=""
-            id="checked-toggle"
-            class="sr-only peer"
-             v-model="DarkMode"
-            checked
-          />
-          <div
-            class="
-              w-11
-              h-6
-              bg-gray-200
-              rounded-full
-              peer
-              peer-focus:ring-4 peer-focus:ring-blue-300
-              dark:peer-focus:ring-blue-800 dark:bg-gray-700
-              peer-checked:after:translate-x-full
-              peer-checked:after:border-white
-              after:content-['']
-              after:absolute
-              after:top-0.5
-              after:left-[2px]
-              after:bg-white
-              after:border-gray-300
-              after:border
-              after:rounded-full
-              after:h-5
-              after:w-5
-              after:transition-all
-              dark:border-gray-600
-              peer-checked:bg-blue-600
-            "
-          ></div>
-        </label>
+        <div>
+          <button class="pt-2">
+            <svg
+              @click="DarkMode = false"
+              v-if="DarkMode"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6 text-white hover:text-gray-400"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+              />
+            </svg>
+            <svg
+              v-else
+              @click="DarkMode = true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6 text-black hover:text-gray-400"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+              />
+            </svg>
+          </button>
+        </div>
       </div>
     </div>
   </div>
@@ -253,7 +250,7 @@ export default {
     return {
       show: false,
       selectedLang: "BG",
-      DarkMode:false,
+      DarkMode: false,
       lang: [
         {
           name: "BG",
@@ -294,7 +291,6 @@ export default {
       this.DarkMode = true;
     }
   },
-
 };
 </script>
 
