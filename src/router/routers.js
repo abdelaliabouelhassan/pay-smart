089b3c2a6f1ee1ffc9932b739ddf22af1d0f1dc9
abdelaliabouelhassan <abdelaliabouelhassan@gmail.com>
2022-10-15 18:07:46 +0100

    scroll behavor

--- src/router/routers.js
+++ src/router/routers.js
@@ -25,6 +25,10 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),
     routes,
+    scrollBehavior(to, from, savedPosition) {
+      // always scroll to top smooth
+      return { top: 0, behavior: 'smooth' }
+    },
   })
 
 export default router
