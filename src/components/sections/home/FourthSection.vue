3bfd1ede5752ada8511701ee3452223e6576c95f
abdelaliabouelhassan <abdelaliabouelhassan@gmail.com>
2022-10-24 13:18:22 +0100

    changes

--- src/components/sections/home/FourthSection.vue
+++ src/components/sections/home/FourthSection.vue
@@ -1,6 +1,20 @@
 <template>
-  <section class="w-full flex bg-[#0F2138] dark:bg-[#1F1F1F] md:pt-28 pt-14 md:pb-52 pb-14 font-Inter  relative">
-    <div class="m-auto w-full max-w-[1150px] p-6 ">
+  <section
+    class="
+      w-full
+      bg-[#0F2138]
+      dark:bg-[#1F1F1F]
+      md:pt-28
+      pt-14
+      md:pb-52
+      pb-14
+      font-Inter
+      relative
+      hidden
+      dark:flex
+    "
+  >
+    <div class="m-auto w-full max-w-[1150px] p-6">
       <div class="w-full grid grid-cols-1 lg:grid-cols-2">
         <div class="flex flex-col items-start space-y-10 w-full">
           <h1 class="font-semibold md:text-5xl text-[32px] text-white">
@@ -9,12 +23,30 @@
           <div class="flex flex-col items-start space-y-4">
             <div class="bg-[#1B2C42] dark:bg-[#272727] rounded-3xl p-5">
               <div class="flex items-start space-x-5">
-                <div class="bg-[#0054B8] dark:bg-[#2B71C4] w-[50px] h-[50px] rounded-full flex">
-                  <span class="m-auto md:text-2xl text-lg font-semibold text-white"
+                <div
+                  class="
+                    bg-[#0054B8]
+                    dark:bg-[#2B71C4]
+                    w-[50px]
+                    h-[50px]
+                    rounded-full
+                    flex
+                  "
+                >
+                  <span
+                    class="m-auto md:text-2xl text-lg font-semibold text-white"
                     >01</span
                   >
                 </div>
-                <div class="flex flex-col items-start space-y-2 max-w-[230px] md:max-w-[276px]">
+                <div
+                  class="
+                    flex flex-col
+                    items-start
+                    space-y-2
+                    max-w-[230px]
+                    md:max-w-[276px]
+                  "
+                >
                   <span class="text-2xl font-semibold text-white"
                     >Create a Free Account</span
                   >
@@ -27,7 +59,8 @@
             <div class="rounded-3xl p-5">
               <div class="flex items-center space-x-5">
                 <div class="bg-[#3A495C] w-[50px] h-[50px] rounded-full flex">
-                  <span class="m-auto  md:text-2xl text-lg font-semibold text-white"
+                  <span
+                    class="m-auto md:text-2xl text-lg font-semibold text-white"
                     >02</span
                   >
                 </div>
@@ -41,7 +74,8 @@
             <div class="rounded-3xl p-5">
               <div class="flex items-center space-x-5">
                 <div class="bg-[#3A495C] w-[50px] h-[50px] rounded-full flex">
-                  <span class="m-auto md:text-2xl text-lg font-semibold text-white"
+                  <span
+                    class="m-auto md:text-2xl text-lg font-semibold text-white"
                     >03</span
                   >
                 </div>
@@ -58,7 +92,8 @@
               space-x-4
               py-3
               px-6
-              bg-[#0054B8] dark:bg-[#2B71C4]
+              bg-[#0054B8]
+              dark:bg-[#2B71C4]
               hover:bg-opacity-80
               rounded-full
             "
@@ -84,10 +119,255 @@
             </div>
           </button>
         </div>
-        <div class=" w-full hidden lg:block">
-            <div class=" absolute right-0 bottom-0 max-w-[600px] xl:max-w-[705px] h-[676px] w-full overflow-hidden bg-card " >
-                  <img src="../../../assets/chartcard.png" class=" w-full h-full object-cover" alt=""> 
+        <div class="w-full hidden lg:block">
+          <div
+            class="
+              absolute
+              right-0
+              bottom-0
+              max-w-[600px]
+              xl:max-w-[705px]
+              h-[676px]
+              w-full
+              overflow-hidden
+              bg-card
+            "
+          >
+            <img
+              src="../../../assets/chartcard.png"
+              class="w-full h-full object-cover"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section
+    class="
+      w-full
+      flex
+      bg-white
+      dark:bg-[#1F1F1F]
+      md:pt-28
+      pt-14
+      md:pb-14
+      pb-14
+      font-Inter
+      relative
+      dark:hidden
+    "
+  >
+    <div class="m-auto w-full max-w-[1150px] p-6">
+      <div class="w-full flex md:flex-row flex-col  space-y-10 items-center md:justify-between">
+        <div class="flex flex-col items-start space-y-7 md:w-full">
+          <h1
+            class="
+              font-semibold
+              md:text-5xl
+              flex flex-col
+              items-start
+              space-y-2
+              text-[32px] text-[#1d1d1d]
+            "
+          >
+            <span class="text-[#0055b7]">Get Started</span>
+            <span> in minutes</span>
+          </h1>
+
+          <button
+            class="
+              flex
+              items-center
+              space-x-4
+              py-3
+              px-6
+              bg-[#0054B8]
+              dark:bg-[#2B71C4]
+              hover:bg-opacity-80
+              rounded-full
+            "
+          >
+            <span class="font-semibold text-base font-Inter text-white"
+              >Get Started</span
+            >
+            <div>
+              <svg
+                width="10"
+                height="18"
+                viewBox="0 0 10 18"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M0.292893 0.292893C0.683417 -0.0976311 1.31658 -0.0976311 1.70711 0.292893L9.70711 8.29289C10.0976 8.68342 10.0976 9.31658 9.70711 9.70711L1.70711 17.7071C1.31658 18.0976 0.683417 18.0976 0.292893 17.7071C-0.0976311 17.3166 -0.0976311 16.6834 0.292893 16.2929L7.58579 9L0.292893 1.70711C-0.0976311 1.31658 -0.0976311 0.683417 0.292893 0.292893Z"
+                  fill="white"
+                />
+              </svg>
             </div>
+          </button>
+        </div>
+        <div class="w-full flex flex-col items-start space-y-4">
+          <div
+            class="
+              hover:bg-white
+              border
+              dark:bg-[#26272C] dark:border-[#343434]
+              rounded-3xl
+              border-[#EEEEEE]
+              hover:shadow-card
+              cursor-pointer
+              w-full
+             
+              md:shadow-none
+              shadow-card
+              px-10 py-5
+            "
+          >
+            <div class="flex items-start space-x-10">
+              <div class="w-[50px] h-[50px] rounded-full flex">
+                <span class="m-auto text-5xl font-semibold text-[#555555]"
+                  >01</span
+                >
+              </div>
+              <div class="flex flex-col items-start space-y-2 md:max-w-[393px]">
+                <span
+                  class="
+                    text-2xl
+                    font-semibold
+                    dark:text-white
+                    text-[#1B2C42]
+                    leading-[36px]
+                    max-w-[221px]
+                    md:max-w-max
+                  "
+                  >Create a Free Account</span
+                >
+                <p
+                  class="
+                    md:text-lg
+                    text-sm
+                    leading-[24px]
+                    md:leading-[30px]
+                    font-medium
+                    dark:text-[#C3C8CD] dark:md:text-[#A9AFB7]
+                    text-[#616C7C]
+                    max-w-[221px]
+                    md:max-w-max
+                  "
+                >
+                  Create your account and follow the onboarding step by step
+                </p>
+              </div>
+            </div>
+          </div>
+          <div
+            class="
+              hover:bg-white
+              border
+              dark:bg-[#26272C] dark:border-[#343434]
+              rounded-3xl
+              border-[#EEEEEE]
+              hover:shadow-card
+              cursor-pointer
+              w-full
+             
+             
+           
+              px-10 py-6
+            "
+          >
+            <div class="flex items-start space-x-10">
+              <div class="w-[50px] h-[50px] rounded-full flex">
+                <span class="m-auto text-5xl font-semibold text-[#555555]"
+                  >02</span
+                >
+              </div>
+              <div class="flex flex-col items-start space-y-2 md:max-w-[300px]">
+                <span
+                  class="
+                    text-2xl
+                    font-semibold
+                    dark:text-white
+                    text-[#1B2C42]
+                    leading-[36px]
+                    max-w-[221px]
+                    md:max-w-max
+                  "
+                  >Verify your identity</span
+                >
+                <p
+                  class="
+                    md:text-lg
+                    text-sm
+                    leading-[24px]
+                    md:leading-[30px]
+                    font-medium
+                    dark:text-[#C3C8CD] dark:md:text-[#A9AFB7]
+                    text-[#616C7C]
+                    max-w-[221px]
+                    md:max-w-max
+                  "
+                >
+                  Once you ve onboarded, verify your identity
+                </p>
+              </div>
+            </div>
+          </div>
+          <div
+            class="
+              hover:bg-white
+              border
+              dark:bg-[#26272C] dark:border-[#343434]
+              rounded-3xl
+              border-[#EEEEEE]
+              hover:shadow-card
+              cursor-pointer
+              w-full
+              
+             
+              px-10 py-6
+            "
+          >
+            <div class="flex items-center space-x-10 w-full">
+              <div class="w-[50px] h-[50px] rounded-full flex">
+                <span class="m-auto text-5xl font-semibold text-[#555555]"
+                  >03</span
+                >
+              </div>
+              <div class="flex flex-col items-start space-y-2 w-full">
+                <span
+                  class="
+                    text-2xl
+                    font-semibold
+                    dark:text-white
+                    text-[#1B2C42]
+                    leading-[36px]
+                    max-w-[221px]
+                    md:max-w-max
+                  "
+                  >All Set</span
+                >
+                <p
+                  class="
+                    md:text-lg
+                    text-sm
+                    leading-[24px]
+                    md:leading-[30px]
+                    font-medium
+                    dark:text-[#C3C8CD] dark:md:text-[#A9AFB7]
+                    text-[#616C7C]
+                    max-w-[221px]
+                    md:max-w-max
+                  "
+                >
+                  Use Wallex all in one
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -97,7 +377,8 @@
 
 
 <style scoped>
- .bg-card {
-  background: linear-gradient(134.66deg, #5CBFEA -5.32%, #1A64BD 99.69%);border-radius: 60px 0px 0px 0px;
- }
+.bg-card {
+  background: linear-gradient(134.66deg, #5cbfea -5.32%, #1a64bd 99.69%);
+  border-radius: 60px 0px 0px 0px;
+}
 </style>
